064be43fccbb49e43ae175f779f54bb9d7054af7
andreghanima <Andreghanima@gmail.com>
2020-10-08 21:42:41 +0200

    Added route button for movies.

--- MovieRental/Data/MovieContext.cs
+++ MovieRental/Data/MovieContext.cs
@@ -1,10 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using MovieRental.Common;
 using MovieRental.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace MovieRental.Data
 {
